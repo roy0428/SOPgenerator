0ba99a4625dc1785aa309c2fe884939b022106c6
mx <72844883+lolzpo@users.noreply.github.com>
2022-03-03 02:03:29 +0800

    Add files via upload

--- grap_filename.bat
+++ grap_filename.bat
@@ -1,16 +1,16 @@
 @echo off
 
 set /P file_name=please enter filename.
-::set bag=.bag
-::set avi=.avi
+set bag=.bag
+set avi=.avi
 
-::python ./extract_bag.py ./%file_name%%bag%
-::timeout 5
+python ./extract_bag.py ./%file_name%%bag%
+timeout 5
 
-::cd ./%file_name%
-::dir /b color > filenames.csv
-::cd ..
+cd ./%file_name%
+dir /b color > filenames.csv
+cd ..
 
 python ./c_excel.py ./%file_name%/filenames.csv ./%file_name%/sample.csv
-::python ./images_to_video.py ./%file_name%/color/ ./%file_name%/%file_name%%avi%
+python ./images_to_video.py ./%file_name%/color/ ./%file_name%/%file_name%%avi%
 python ./check_elements.py ./%file_name%/filenames.csv ./%file_name%/sample.csv ./%file_name%/elements.csv
